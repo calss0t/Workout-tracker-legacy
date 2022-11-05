cb06888a245cb17b394f968ef4ed5b929f0bdb6c
Pol Texido <107403548+calss0t@users.noreply.github.com>
2022-11-05 11:37:52 +0900

    Final commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,17 +7,17 @@ import { useState, useEffect } from 'react';
 function App() {
   const [displayProfile, setDisplayProfile] = useState(false);
   const [view, setView] = useState(null);
-  const [date, setDate] = useState("")
-
+  const [date, setDate] = useState(new Date().toDateString())
 
   useEffect(() => {
-    const loginDate = new Date().toDateString()
-    setDate(loginDate)
-    if (localStorage.getItem('userid')) {
-      setView(<WeekView setView={setView}></WeekView>);
-    } else {
-      setView(<LoginSignup date={date} setDate={setDate} setView={setView}></LoginSignup>);
-    }
+    setView(<LoginSignup date={date} setDate={setDate} setView={setView}></LoginSignup>);
+    // const loginDate = new Date().toDateString()
+    // setDate(loginDate)
+    // if (localStorage.getItem('userid')) {
+    //   setView(<WeekView setView={setView}></WeekView>);
+    // } else {
+    //   setView(<LoginSignup date={date} setDate={setDate} setView={setView}></LoginSignup>);
+    // }
   }, []);
 
   return (
